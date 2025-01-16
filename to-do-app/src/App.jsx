11c3e8f99fbbcbe409c8e4eec697c8fc9e602633
kachakaran6 <kachakaran06@gmail.com>
2025-01-16 17:54:25 +0530

    first commit

---
+++ to-do-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from './components/Todo'
+
+const App = () => {
+  return (
+    <div className='bg-[#11131e] grid py-4 min-h-screen fontFamily-Roboto sans-serif' >
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
